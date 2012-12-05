39e01a48f733f340393e9339bee25e9abe2a94a7
David Vrensk <david@vrensk.com>
2012-12-05 18:40:42 +0100

    cleanup

--- GildedRose/ruby/gilded_rose_spec.rb
+++ GildedRose/ruby/gilded_rose_spec.rb
@@ -1,13 +1,14 @@
-    
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 
 describe GildedRose do
 
- describe "#update_quality" do
-   items = [Item.new("foo", 0, 0)]
-   GildedRose.new().update_quality(items)
-   items[0].name.should == "fixme"
- end
-
+  describe "#update_quality" do
+    it "works" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new().update_quality(items)
+      items[0].name.should == "apa"
+    end
+  end
+    
 end
